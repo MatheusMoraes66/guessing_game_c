3f6c3ecd5756c35da91c6f26f76c1a22d560c53e
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-04-01 14:01:30 -0300

    create header to error messages

---
+++ includes/error_messages.h
@@ -0,0 +1,10 @@
+// error_messages.h
+
+#ifndef ERROR_MESSAGES_H
+#define ERROR_MESSAGES_H
+
+#define ERR_INVALID_NUMBER "O numero colocado está fora das opções."
+#define ERR_INVALID_INPUT                                                      \
+  "Entrada inválida. Por favor, digite um número inteiro."
+
+#endif
